1a37de92a5b29ab4de7eb3d529c2a74b4eae762f
Sarah Verkade <sarahverkade@live.nl>
2021-02-28 14:39:52 +0100

    added template engine

---
+++ index.js
@@ -0,0 +1,44 @@
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const users = {
+    name: "sarah",
+    age: "25",
+  };
+
+// view engine ejs
+app.set('view engine', 'ejs')
+
+// access path public folder
+app.use(express.static('public'));
+
+// middleware make data available in req.body (still figuring out)
+app.use(bodyParser.urlencoded({extended: true}))
+
+// app listens to port 3000
+app.listen(3000);
+
+
+app.use("/home", (req, res, next) => {
+    res.render("home.ejs", { layout: false, users: users, title: "Users" }); // we dont need to use the extention express knows that we use pug templating engine
+  });
+
+
+app.get('/home', function (req, res) {
+    res.render('home.ejs');
+  });
+
+app.get('/profile', function (req, res) {
+    res.render('profile.ejs');
+  });
+
+  app.get('/login', function (req, res) {
+    res.render('login.ejs');
+  });
+
+// Function error 404
+app.use(function (req, res, next){
+    res.status(404).send("Sorry! This page doesn't exist")
+});
+
