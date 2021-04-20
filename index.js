2daf5b9c2816d37159191605b706222b4eefc1be
Sarah Verkade <sarahverkade@live.nl>
2021-04-20 12:41:58 +0200

    changes folder structure

--- index.js
+++ index.js
@@ -1,63 +1,103 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
-const slug = require("slug");
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
 const path = require("path");
+// const slug = require("slug");
 
-const users = {
-  name: "Matthew",
-  age: "28",
-};
 
-// view engine ejs
+// ========================
+// Middlewares
+// ========================
 app.set("view engine", "ejs");
-
-// access path public folder
+// Read data from body
+app.use(bodyParser.urlencoded({ extended: true }));
+// Access public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// middleware make data available in req.body (still figuring out)
-app
-  .use(bodyParser.urlencoded({ extended: true }))
 
-  .post("/", add)
-  .get("/home", form);
+// ========================
+// Database connections
+// ========================
+// Use dontenv npm package 
+require("dotenv").config();
+// Access environment variables
+const dotenv = require("dotenv");
 
-function form(req, res) {
-  res.render("home.ejs");
-}
-function add(req, res) {
-  const id = slug(req.body.title).toLowerCase();
-
-  data.push({
-    title: req.body.title,
-    plot: req.body.plot,
-    description: req.body.description,
-  });
-  res.redirect("/");
-}
+// Process URI
+const uri = process.env.DB_URI;
+const dbName = process.env.DB_NAME;
 
 
-// app listens to port 3000
-app.listen(3000);
-
-// This is a test
-app.use("/profile", (req, res, next) => {
-  res.render("profile.ejs", { layout: false, users, title: "Users" });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.get("/home", function (req, res) {
-  res.render("home.ejs");
+async function main() {
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+      app.listen(3000);
+      console.log("connected to mongo");
+      const db = client.db(dbName);
+      // Make the appropriate DB calls
+      await listDatabases(client);
+      await createUser(
+        client,
+        {
+          name: "matthew",
+          age: "28",
+          Preference: ""
+        }
+      );
+      // catch error
+  } catch (e) {
+      console.error(e);
+  } //finally {
+      // Close the connection to the MongoDB cluster
+    // await client.close();
+ // }
+}
+
+main().catch(console.error);
+
+async function createUser(client, newUser){
+  const result = await client.db("preference").collection("user").insertOne(newUser);
+  console.log(` - ${newUser.preference}`);
+}
+
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+
+// ========================
+// Routes
+// ========================
+
+app.get("/preference", function (req, res) {
+  res.render("preferen.ejs");
 });
 
 app.get("/profile", function (req, res) {
-  res.render("profile.ejs");
+  res.render("profile.ejs", { newUser: newUser.name });
 });
 
-app.get("/login", function (req, res) {
-  res.render("login.ejs");
+app.get("/matches", function (req, res) {
+  res.render("home.ejs");
 });
 
 // Function error 404
 app.use(function (req, res, next) {
   res.status(404).send("Sorry! This page doesn't exist");
 });
+
+
+
+
+
